9c8cd646c3f725d9e0777b3a795122853e63fac6
minseok kyeon <msky0912@gmail.com>
2025-02-03 14:52:43 +0900

    typeconverter study project setting

---
+++ src/main/java/hello/typeconverter_study/TypeconverterStudyApplication.java
@@ -0,0 +1,13 @@
+package hello.typeconverter_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeconverterStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypeconverterStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/typeconverter_study/TypeconverterStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.typeconverter_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeconverterStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
