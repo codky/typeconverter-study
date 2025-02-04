b788c65248a4132a8f08237d0578ec063c5ead03
minseok kyeon <msky0912@gmail.com>
2025-02-04 13:55:24 +0900

    웹 애플리케이션에 포맷터 적용
     - WebConfig 에 MyNumberFormatter() 등록

--- src/main/java/hello/typeconverter_study/WebConfig.java
+++ src/main/java/hello/typeconverter_study/WebConfig.java
@@ -4,6 +4,7 @@ import hello.typeconverter_study.converter.IntegerToStringConverter;
 import hello.typeconverter_study.converter.IpPortToStringConverter;
 import hello.typeconverter_study.converter.StringToIntegerConverter;
 import hello.typeconverter_study.converter.StringToIpPortConverter;
+import hello.typeconverter_study.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,9 +14,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        //주석처리 우선순위: 컨버터와 포매터의 기능이 겹칠 때 컨버터가 우선으로 적용되므로 주석처리 한다.
+        //registry.addConverter(new StringToIntegerConverter());
+        //registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new IpPortToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
+
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
